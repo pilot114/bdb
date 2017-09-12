0c86ef6d65d1e9c8d560f533cb9beb7492e02ca3
Oleg Chulkov <o.chulkov@zarplata.ru>
2017-09-12 19:59:51 +0700

    up

--- bin/vk_examples/vk.php
+++ bin/vk_examples/vk.php
@@ -5,8 +5,17 @@ include './vendor/autoload.php';
 $accessToken = file_get_contents('at.txt');
 $vk = new \Bdb\Addons\VK\Api($accessToken);
 
-$response = $vk->users()->get()
-    ->_user_ids(range(1,10))
+$response = $vk->users()->search()
+	->_city(99)
+	->_fields([
+		'bdate', 'photo_200_orig', 'lists', 'domain', 'contacts', 'site', 'education', 'universities',
+		'schools', 'status', 'last_seen', 'followers_count', 'common_count', 'occupation',
+		'nickname', 'relatives', 'relation', 'personal', 'connections', 'exports', 'wall_comments',
+		'activities', 'interests', 'music', 'movies', 'tv', 'books', 'games', 'about', 'quotes',
+		'can_post', 'can_see_all_posts', 'can_see_audio', 'can_write_private_message', 'can_send_friend_request', 'is_favorite',
+		'is_hidden_from_feed', 'timezone', 'screen_name', 'maiden_name', 'is_friend', 'friend_status', 'career', 'military'
+	])
+    ->_q('Иван Петров')
     ->call();
 
 $result = json_decode((string)$response->getBody());
